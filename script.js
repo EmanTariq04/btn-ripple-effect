0cf7d1aec2888feacf344fdb643da1db327b96d4
Eman Tariq <emantariq.1004@gmail.com>
2025-06-20 20:59:37 +0500

    completed

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const btnEl = document.querySelector(".btn");
 
 btnEl.addEventListener("mouseover", (e) => {
-    console.log(e.pageX = btnEl.offsetLeft);
+    const x = (e.pageX - btnEl.offsetLeft);
+    const y = (e.pageY - btnEl.offsetTop);
+
+    btnEl.style.setProperty("--xPos", x + "px")
+    btnEl.style.setProperty("--yPos", y + "px");
+
 })
